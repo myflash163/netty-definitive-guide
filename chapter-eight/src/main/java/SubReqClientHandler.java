d8951861e7ccd1e87077c9a97e67bcc7aabb478b
myflash163 <myflash2012@gmail.com>
2018-01-19 20:48:04 +0800

    第八章

---
+++ chapter-eight/src/main/java/SubReqClientHandler.java
@@ -0,0 +1,44 @@
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubReqClientHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.write(subReq(i));
+        }
+        ctx.flush();
+    }
+
+    private SubscribeReqProto.SubscribeReq subReq(int i) {
+        SubscribeReqProto.SubscribeReq.Builder builder = SubscribeReqProto.SubscribeReq.newBuilder();
+        builder.setSubReqID(i);
+        builder.setUserName("Lilinfeng");
+        builder.setProductName("Netty Book For Protobuf");
+        List<String> address = new ArrayList<>();
+        address.add("Nanjing YuHuaTai");
+        address.add("BeiJing LiuLiChang");
+        address.add("ShenZhen HongShuLin");
+        builder.addAllAddress(address);
+        return builder.build();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Receive server response : [" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
